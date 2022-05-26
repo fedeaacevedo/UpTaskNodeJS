c04b6e34122cc2c89fcb6e17a82ee6e15f69c8b6
Federico <fedeaacevedo@gmail.com>
2022-05-26 00:16:55 -0300

    Controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//importamos el controlador
+const proyectosController = require('../controllers/proyectosController')
+
 module.exports = function () {
     //ruta para el home
-    router.get('/', (req, res) => {
-        res.send("Hola mundo")
-    })
+    router.get('/', proyectosController.proyectosHome )
     return router;
 }
